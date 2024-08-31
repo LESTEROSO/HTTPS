864ff8260894ad5be32800101077c4c22989f99c
lesmarti <lesteromh@gmail.com>
2024-08-31 17:10:03 -0600

    Final

---
+++ ConsAlumnos/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StudentForm from './studiante';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
